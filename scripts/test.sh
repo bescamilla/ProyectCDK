6e06d479542fecceb131578fb35f03fb9b53324c
root <root@pc0054.fojal.loc>
2021-10-09 11:25:20 -0500

    kk

---
+++ scripts/test.sh
@@ -0,0 +1,9 @@
+echo "Mostrando GET /"
+curl http://localhost:3000
+
+printf "\n\n"
+echo "Mostrando GET /saludo/<nombre>"
+curl http://localhost:3000/saludo/JUAN
+
+printf "\n\n"
+echo "Fin de las pruebas"
